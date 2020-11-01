5cca7dc12a622345f07ca5768464d94317cdfbca
Silvano Gomes Do Amaral <silvano.amaral9@gmail.com>
2020-10-31 22:59:28 -0300

    feature: aplicando ThemeProvider - componente recebe theme como props

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+const colors = {
+  primary: "#499d71",
+  secondary: "#384eca",
+  warning: "yellow",
+  danger: "red",
+  info: "blue",
+  success: "green",
+  outline: "#000"
+}
+
+const space = [0, 4, 8, 16, 32, 64, 128, 256, 512]
+
+export default {
+  colors,
+  space,
+}
